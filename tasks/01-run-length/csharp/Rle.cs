4427a3bcf4c04f9b32043184c8674a037f74c01d
Min Htet Khaing <131604216+MinHtetKhaing1999@users.noreply.github.com>
2025-06-23 13:32:54 +0700

    task 1,2 and 3

--- tasks/01-run-length/csharp/Rle.cs
+++ tasks/01-run-length/csharp/Rle.cs
@@ -1,3 +1,4 @@
+using System.Text;
 namespace Rle;
 
 public static class Encoder
@@ -6,6 +7,20 @@ public static class Encoder
     public static string Encode(string input)
     {
         // TODO: implement
-        throw new NotImplementedException("Implement me!");
+        //throw new NotImplementedException("Implement me!");
+        List<Rune> current = new();
+        string data = string.Empty;
+
+        foreach (Rune rune in input.EnumerateRunes())
+        {
+            if (current.Count > 0 && current[0] != rune)
+            {
+                data += string.Concat(current[0].ToString(), current.Count);
+                current.Clear();
+            }
+            current.Add(rune);
+        }
+        data += current.Any() ? string.Concat(current[0].ToString(), current.Count) : input;
+        return data;
     }
 }
--- tasks/02-fix-the-bug/csharp/BuggyLib/BuggyCounter.cs
+++ tasks/02-fix-the-bug/csharp/BuggyLib/BuggyCounter.cs
@@ -1,14 +1,20 @@
+using System.Threading;
+
 namespace BuggyLib;
 
 public static class BuggyCounter
 {
     private static long _current = 0;
+    private static object _lock = new object();
 
     public static long NextId()
     {
-        long value = _current;
-        System.Threading.Thread.Sleep(0);
-        _current++;
-        return value;
+        lock (_lock)
+        {
+            long value = _current;
+            Thread.Sleep(0);
+            _current++;
+            return value;
+        }
     }
 }
